b48dc77c0285e8d301d42e67eb2d003d6982fb73
Anushka Singh <168325398+01AnushkaSingh@users.noreply.github.com>
2026-02-04 12:05:17 +0530

    Time: 159 ms (99.74%) | Memory: 31.2 MB (99.35%) - LeetSync

--- 3956-trionic-array-ii/trionic-array-ii.py
+++ 3956-trionic-array-ii/trionic-array-ii.py
@@ -1,24 +1,40 @@
+class Solution:
+    def maxSumTrionic(self, nums: List[int]) -> int:
+        n = len(nums)
+        i = 0
+        ans = -inf
+        while i < n:
+            l = i
+            i += 1
+            while i < n and nums[i - 1] < nums[i]:
+                i += 1
+            if i == l + 1:
+                continue
 
-class Solution(object):
-    def maxSumTrionic(self, nums):
-       
-        result = float("-inf")
-        left = p = q = 0
-        prefix = nums[0]
-        for right in range(1, len(nums)):
-            prefix += nums[right]
-            if nums[right-1] > nums[right]:
-                if right-2 >= 0 and nums[right-2] < nums[right-1]:
-                    p = right-1
-                    while left < q or (nums[left] < 0 and left+1 < p):
-                        prefix -= nums[left]
-                        left += 1
-            elif nums[right-1] < nums[right]:
-                if right-2 >= 0 and nums[right-2] > nums[right-1]:
-                    q = right-1
-                if left != p:
-                    result = max(result, prefix)
-            else:
-                left = p = q = right
-                prefix = nums[right]
-        return result
+            p = i - 1
+            s = nums[p - 1] + nums[p]
+            while i < n and nums[i - 1] > nums[i]:
+                s += nums[i]
+                i += 1
+            if i == p + 1 or i == n or nums[i - 1] == nums[i]:
+                continue
+
+            q = i - 1
+            s += nums[i]
+            i += 1
+            mx = t = 0
+            while i < n and nums[i - 1] < nums[i]:
+                t += nums[i]
+                i += 1
+                mx = max(mx, t)
+            s += mx
+
+            mx = t = 0
+            for j in range(p - 2, l - 1, -1):
+                t += nums[j]
+                mx = max(mx, t)
+            s += mx
+
+            ans = max(ans, s)
+            i = q
+        return ans
