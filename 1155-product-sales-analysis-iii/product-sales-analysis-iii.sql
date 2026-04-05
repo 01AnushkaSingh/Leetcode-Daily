62d169239ca8da2efe3bb2ff59b29c1aa249db70
Anushka Singh <168325398+01AnushkaSingh@users.noreply.github.com>
2026-04-05 12:31:15 +0530

    Time: 675 ms (69.92%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
@@ -0,0 +1,16 @@
+# Write your MySQL query statement below
+
+
+WITH
+    T AS (
+        SELECT
+            *,
+            RANK() OVER (
+                PARTITION BY product_id
+                ORDER BY year
+            ) AS rk
+        FROM Sales
+    )
+SELECT product_id, year AS first_year, quantity, price
+FROM T
+WHERE rk = 1;
