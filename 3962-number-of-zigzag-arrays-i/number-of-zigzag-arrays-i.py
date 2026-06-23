599f7fc60aeb6d29b1d18832e21f928caede3496
Anushka Singh <168325398+01AnushkaSingh@users.noreply.github.com>
2026-06-23 09:20:03 +0530

    Time: 3683 ms (89.47%) | Memory: 19.3 MB (89.47%) - LeetSync

---
+++ 3962-number-of-zigzag-arrays-i/number-of-zigzag-arrays-i.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def zigZagArrays(self, n, l, r):
+      
+        MOD = 10**9+7
+        r -= l
+        dp = [1]*(r+1)
+        for _ in range(n-1):
+            prefix = 0
+            for i in range(len(dp)):
+                dp[i], prefix = prefix, (prefix+dp[i])%MOD
+            dp.reverse()
+        return (reduce(lambda accu, x: (accu+x)%MOD, dp, 0)*2)%MOD
